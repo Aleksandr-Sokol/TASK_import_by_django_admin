0838ec087497857327c9738795a2c8b9a2c3ea64
Aleksandr-Sokol <74308502+Aleksandr-Sokol@users.noreply.github.com>
2022-02-06 08:37:27 +0500

    feat: Add models for data

--- django_admin_import_excel/import_data/models.py
+++ django_admin_import_excel/import_data/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.db.models import ForeignKey, PROTECT
+
+
+class Zmk(models.Model):
+    name = models.CharField(max_length=120, null=False)
 
 
 class Registry(models.Model):
+    num = models.PositiveIntegerField()
     name = models.CharField(max_length=120, default='')
+    weight = models.FloatField()
+    departure_date = models.DateField(null=True, blank=True)
+    receiving_date = models.DateField(null=True, blank=True)
+    zmk = ForeignKey(Zmk,
+                     related_name='rts',
+                     on_delete=PROTECT,
+                     null=True,
+                     )
