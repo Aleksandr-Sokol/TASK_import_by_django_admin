e0d10477ef91828f9c96ef44a9593703727f4e31
Aleksandr-Sokol <74308502+Aleksandr-Sokol@users.noreply.github.com>
2022-02-06 07:38:53 +0500

    feat: Add function to import excel file and parsing file

--- django_admin_import_excel/import_data/admin.py
+++ django_admin_import_excel/import_data/admin.py
@@ -6,6 +6,40 @@ import sys
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Registry
 from django.urls import path, reverse
+import pandas as pd
+import xlrd
+
+
+def parsing_excel(df) -> dict:
+    """
+    :param df - pandas dataframe from excel file
+    return dict data
+    Функция очищает пустый столбики данных и столбики в которых не заполнены поля даты
+    """
+    result_dict = {}
+    row_names = df.iloc[1]
+    zmk_has_names = pd.isnull(row_names)
+    zmk_names = [name for name, has_name in zip(row_names.tolist(), zmk_has_names) if not has_name]
+    df_start_position = [i + 1 for i, value in enumerate((pd.isnull(df.iloc[3])).tolist()) if value]
+    df_start_position.append(df.shape[1] + 1)
+    rows_count = df.shape[0]
+    rows = [i for i in range(4, rows_count)]
+    for num, index in enumerate(df_start_position[:-1]):
+        result_dict[zmk_names[num]] = {}
+        end = df_start_position[num + 1] - 1
+        columns = [i for i in range(index, end)]
+        column_names = (df.iloc[3]).tolist()[index: end]
+        columns_count = len(columns)
+        objects_count = columns_count - 5
+        objects_nums = [i for i in range(2, objects_count + 2)]
+        df0 = df.iloc[rows, columns]
+        df_i = df0.dropna(thresh=3)  # нужно хотя бы 3 значения в строке
+        for object_num in objects_nums:
+            result_dict[zmk_names[num]][column_names[object_num]] = []
+            df_i_obj = (df_i.iloc[:, [0, 1, object_num, columns_count - 1]]).dropna(thresh=4)
+            for i, row in df_i_obj.iterrows():
+                result_dict[zmk_names[num]][column_names[object_num]].append(row.tolist())
+    return result_dict
 
 
 class CsvImportForm(forms.Form):
@@ -26,17 +60,11 @@ class RegistryAdmin(admin.ModelAdmin):
     def import_csv(self, request):
         if request.method == "POST":
             file = request.FILES["xlsx_file"]
-            reader = csv.reader(file)
-            print(file)
-            print(file.name)
-            print(reader)
-            # with open(file.name) as f:
-            #     lines = f.readlines()
-            # for line in lines:
-            #     print(line)
-            # Create Hero objects from passed in data
-            # ...
-            self.message_user(request, "Your xlsx file has been imported")
+            df = pd.read_excel(file.file)
+            dict_data = parsing_excel(df)
+            print(dict_data)
+
+            self.message_user(request, f"Your xlsx file has been imported. new {10}")
             return redirect("..")
         form = CsvImportForm()
         payload = {"form": form}
